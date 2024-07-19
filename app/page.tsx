def93eeeb9ba17e63f73ee864e857006acbacae8
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-19 05:06:49 +0200

    Installed Shadcn for component styling and customization.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-white">Home</h1>
+      <Button></Button>
     </div>
   )
 }
