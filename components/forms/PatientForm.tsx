a2a16c5f6dce510aa904a200d0bbea73342481e7
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-19 10:22:31 +0200

    Installed add form from shadcn to help define the form schema.

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,6 +1,30 @@
-import React from 'react'
-
-const PatientForm = () => {
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+ 
+export function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
     <div>PatientForm</div>
   )
