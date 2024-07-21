39746c62abddea96d4971816834f6a3ebf90bb57
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-21 17:24:46 +0200

    Finished with the ui of the authentication  form. Building validation.

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -41,7 +41,7 @@ const PatientForm = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="spa ce-y-6 flex-1">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
         <section className="mb:12 space-y-4">
             <h1 className="header">
                 Welcome to the Healthy Gang.
