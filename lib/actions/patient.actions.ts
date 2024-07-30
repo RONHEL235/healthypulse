8b3f857b5e126838514c8d7d8721c4baf0d45ad0
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-30 17:40:21 +0200

    Implementing the created user from appwrite.

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,3 +1,15 @@
+import { users } from "../appwrite.config"
+
 export const createUser = async (user: CreateUserParams) => {
-    //creating a form using inputs created in appwrite.
+    try {
+
+    } catch (error) {
+      if (error && error?.code === 409) {
+        const existingUser = await users.list([
+            Query.equal("email", [user.email])
+        ])
+
+        return existing?.users[0]
+      }  
+    }
 }
