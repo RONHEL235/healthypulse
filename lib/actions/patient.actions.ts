c7e7a04a611ffe37456abfceca2329b0e2f63075
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-31 19:15:47 +0200

    Comments

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,7 +2,7 @@ import { users } from "../appwrite.config"
 
 export const createUser = async (user: CreateUserParams) => {
     try {
-
+      //A comment
     } catch (error) {
       if (error && error?.code === 409) {
         const existingUser = await users.list([
